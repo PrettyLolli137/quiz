85088b7e21f5221fe4cfc3ff6b570557cd50181c
PrettyLolli <yunghunters2015@gmail.com>
2024-01-29 11:59:50 +0300

    адаптивка

--- quiz/src/components/App/App.jsx
+++ quiz/src/components/App/App.jsx
@@ -30,19 +30,43 @@ const questions = [
     correct: 2,
   },
   {
-    title: "React - это ... ?",
+    title: "Что такое HTML?",
+    variants: ["HyperText Markup Language",
+      "High-Level Text Management Language",
+      "Hyper Transfer Text Language"],
+    correct: 0,
+  },
+  {
+    title: "Что означает аббревиатура CSS?",
+    variants: ["Computer Style Sheets",
+    "Creative Style Sheets",
+    "Cascading Style Sheets"],
+    correct: 2,
+  },
+  {
+    title: "Какой символ используется для комментариев в большинстве языков программирования?",
     variants: ["библиотека", "фреймворк", "приложение"],
     correct: 0,
   },
   {
-    title: "React - это ... ?",
-    variants: ["библиотека", "фреймворк", "приложение"],
+    title: "Что делает оператор if в языках программирования?",
+    variants: ["//", " #", " --"],
     correct: 0,
   },
   {
-    title: "React - это ... ?",
-    variants: ["библиотека", "фреймворк", "приложение"],
-    correct: 0,
+    title: "Что такое Git?",
+    variants: ["Графический редактор", "Система контроля версий", "Программный язык"],
+    correct: 1,
+  },
+  {
+    title: "Какая структура данных используется для хранения упорядоченной коллекции элементов в JavaScript?",
+    variants: ["Stack", "Queue", "Array"],
+    correct: 2,
+  },
+  {
+    title: "Какой язык программирования был разработан для создания динамических веб-страниц?",
+    variants: ["Python", "JavaScript", "Ruby"],
+    correct: 1,
   },
 ];
 
@@ -64,7 +88,7 @@ function App() {
 
   return (
     <div className="App">
-      {step != questions.length ? (
+      {step !== questions.length ? (
         <Game
           step={step}
           question={question}
